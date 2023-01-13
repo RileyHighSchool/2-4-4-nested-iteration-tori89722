87406a13b71bf6773879a61860d02c78d5457147
tori89722 <victoria89722@students.sbcsc.k12.in.us>
2023-01-13 13:42:03 -0500

    finished letter frequencies

--- LetterFrequencies.java
+++ LetterFrequencies.java
@@ -7,9 +7,29 @@ public class LetterFrequencies
 	{
 		String letters = "abcdefghijklmnopqrstuvwxyz"; 
 		String phrase = "This is a phrase!";
+		
 
 		System.out.println("The following shows the letter frequencies for the phrase");
 
     /* your code here */
+
+
+	for (int i = 0; i <= letters.length(); i++)
+	{
+		String alphaBet = letters.substring(i, i+1);
+		int count = 0;
+
+		for (int n = 0; n < phrase.length(); n++){
+			
+			String phraseLet = phrase.substring(n, n+1);
+
+			
+			if (alphaBet.equals (phraseLet)){
+				count += 1;
+			}
+		}
+		System.out.println(alphaBet + " appears " + count + " times");
+
+	}
 	}
 }
